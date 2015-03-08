3d2311569b9d7b528d7c6fc93527a010d35d8304
AGES5 <dareka3627@gmail.com>
2015-03-08 23:17:56 +0900

    バッチモードビルド設定

---
+++ TCPTest/Assets/BuildSetting/iOS/Dev/BatchBuild.cs
@@ -0,0 +1,65 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using System;
+using UnityEditor;
+
+/// <summary>
+/// バッチモードでビルドを行うクラス.
+/// </summary>
+public class BatchBuild {
+
+	private static string CompanyName = "";
+	private static string ProductName = "";
+	private static string BundleVersion = "";
+	private static string KeyStorePath = "";
+	private static string KeyStorePass = "";
+	private static string KeyAliasName = "";
+	private static string KeyAliasPass = "";
+
+	private static string BuidlePathiOS = "";
+	private static string BuildPathAndroid = "Bin/Android/Project/Dev";
+
+
+	/// <summary>
+	/// ビルドするシーンを取得
+	/// </summary>
+	/// <returns>The scenes.</returns>
+	public static string[] GetScenes() {
+		List<string> scenes = new List<string> ();
+
+		// 
+		// TODO
+		// 
+
+		return scenes.ToArray ();
+	}
+
+	public static void BatchBuildiOS() {
+
+
+	}
+
+	public static void BatchBuildAndroid() {
+
+		// 
+		// TODO keystore系の設定
+		//
+
+		string[] scenes = GetScenes ();
+		// (development かつ Eclipse出力設定)
+		string errorMsg = BuildPipeline.BuildPlayer(scenes, 
+		                                            BuildPathAndroid, 
+		                                            BuildTarget.Android,
+		                                            BuildOptions.Development | BuildOptions.AcceptExternalModificationsToPlayer
+		                                            );
+		if(string.IsNullOrEmpty(errorMsg)) {
+			Debug.Log("BatchBuildAndroid success!");
+		}
+		else {
+			Debug.Log("BatchBuildAndroid failed..");
+		}
+	}
+
+
+}
